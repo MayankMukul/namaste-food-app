a47943f3f74806ccc2349072f1208ce5ee851794
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-10-11 11:58:16 +0530

    Shimmer ui

--- src/Container.js
+++ src/Container.js
@@ -1,217 +1,218 @@
 // import React from 'react';
 
 import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
-const restaurantlist = [
-  {
-    "info": {
-      "id": "614573",
-      "name": "Subway",
-      "cloudinaryImageId": "1ace5fa65eff3e1223feb696c956b38b",
-      "locality": "Sector 11",
-      "areaName": "Rohini",
-      "costForTwo": "₹350 for two",
-      "cuisines": [
-        "Salads",
-        "Snacks",
-        "Desserts",
-        "Beverages"
-      ],
-      "avgRating": 3.9,
-      "feeDetails": {
-        "restaurantId": "614573",
-        "fees": [
-          {
-            "name": "BASE_DISTANCE",
-            "fee": 6300
-          },
-          {
-            "name": "BASE_TIME"
-          },
-          {
-            "name": "ANCILLARY_SURGE_FEE"
-          }
-        ],
-        "totalFee": 6300
-      },
-      "parentId": "2",
-      "avgRatingString": "3.9",
-      "totalRatingsString": "500+",
-      "promoted": true,
-      "adTrackingId": "cid=8774140~p=1~eid=0000018b-0b7d-b6c2-5209-00e000580101~srvts=1696704870082~45995",
-      "sla": {
-        "deliveryTime": 22,
-        "lastMileTravel": 4.8,
-        "serviceability": "SERVICEABLE",
-        "slaString": "22 mins",
-        "lastMileTravelString": "4.8 km",
-        "iconType": "ICON_TYPE_EMPTY"
-      },
-      "availability": {
-        "nextCloseTime": "2023-10-08 01:00:00",
-        "opened": true
-      },
-      "badges": {
-        "textExtendedBadges": [
-          {
-            "iconId": "guiltfree/GF_Logo_android_3x",
-            "shortDescription": "options available",
-            "fontColor": "#7E808C"
-          }
-        ]
-      },
-      "isOpen": true,
-      "type": "F",
-      "badgesV2": {
-        "entityBadges": {
-          "imageBased": {
+// const restaurantlist = [
+//   {
+//     "info": {
+//       "id": "614573",
+//       "name": "Subway",
+//       "cloudinaryImageId": "1ace5fa65eff3e1223feb696c956b38b",
+//       "locality": "Sector 11",
+//       "areaName": "Rohini",
+//       "costForTwo": "₹350 for two",
+//       "cuisines": [
+//         "Salads",
+//         "Snacks",
+//         "Desserts",
+//         "Beverages"
+//       ],
+//       "avgRating": 3.9,
+//       "feeDetails": {
+//         "restaurantId": "614573",
+//         "fees": [
+//           {
+//             "name": "BASE_DISTANCE",
+//             "fee": 6300
+//           },
+//           {
+//             "name": "BASE_TIME"
+//           },
+//           {
+//             "name": "ANCILLARY_SURGE_FEE"
+//           }
+//         ],
+//         "totalFee": 6300
+//       },
+//       "parentId": "2",
+//       "avgRatingString": "3.9",
+//       "totalRatingsString": "500+",
+//       "promoted": true,
+//       "adTrackingId": "cid=8774140~p=1~eid=0000018b-0b7d-b6c2-5209-00e000580101~srvts=1696704870082~45995",
+//       "sla": {
+//         "deliveryTime": 22,
+//         "lastMileTravel": 4.8,
+//         "serviceability": "SERVICEABLE",
+//         "slaString": "22 mins",
+//         "lastMileTravelString": "4.8 km",
+//         "iconType": "ICON_TYPE_EMPTY"
+//       },
+//       "availability": {
+//         "nextCloseTime": "2023-10-08 01:00:00",
+//         "opened": true
+//       },
+//       "badges": {
+//         "textExtendedBadges": [
+//           {
+//             "iconId": "guiltfree/GF_Logo_android_3x",
+//             "shortDescription": "options available",
+//             "fontColor": "#7E808C"
+//           }
+//         ]
+//       },
+//       "isOpen": true,
+//       "type": "F",
+//       "badgesV2": {
+//         "entityBadges": {
+//           "imageBased": {
             
-          },
-          "textExtendedBadges": {
-            "badgeObject": [
-              {
-                "attributes": {
-                  "iconId": "guiltfree/GF_Logo_android_3x",
-                  "description": "",
-                  "shortDescription": "options available",
-                  "fontColor": "#7E808C"
-                }
-              }
-            ]
-          },
-          "textBased": {
+//           },
+//           "textExtendedBadges": {
+//             "badgeObject": [
+//               {
+//                 "attributes": {
+//                   "iconId": "guiltfree/GF_Logo_android_3x",
+//                   "description": "",
+//                   "shortDescription": "options available",
+//                   "fontColor": "#7E808C"
+//                 }
+//               }
+//             ]
+//           },
+//           "textBased": {
             
-          }
-        }
-      },
-      "aggregatedDiscountInfoV3": {
-        "header": "40% OFF",
-        "subHeader": "UPTO ₹80"
-      },
-      "differentiatedUi": {
-        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-        "differentiatedUiMediaDetails": {
-          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
-          "lottie": {
+//           }
+//         }
+//       },
+//       "aggregatedDiscountInfoV3": {
+//         "header": "40% OFF",
+//         "subHeader": "UPTO ₹80"
+//       },
+//       "differentiatedUi": {
+//         "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//         "differentiatedUiMediaDetails": {
+//           "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+//           "lottie": {
             
-          },
-          "video": {
+//           },
+//           "video": {
             
-          }
-        }
-      },
-      "reviewsSummary": {
+//           }
+//         }
+//       },
+//       "reviewsSummary": {
         
-      },
-      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-      "restaurantOfferPresentationInfo": {
+//       },
+//       "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//       "restaurantOfferPresentationInfo": {
         
-      }
-    },
-    "analytics": {
+//       }
+//     },
+//     "analytics": {
       
-    },
-    "cta": {
-      "link": "swiggy://menu?restaurant_id=614573",
-      "text": "RESTAURANT_MENU",
-      "type": "DEEPLINK"
-    }
-  },
-  {
-    "info": {
-      "id": "498382",
-      "name": "Burger King",
-      "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-      "locality": "M2K Mall",
-      "areaName": "Rohini",
-      "costForTwo": "₹350 for two",
-      "cuisines": [
-        "Burgers",
-        "American"
-      ],
-      "avgRating": 4.4,
-      "feeDetails": {
-        "restaurantId": "498382",
-        "fees": [
-          {
-            "name": "BASE_TIME"
-          },
-          {
-            "name": "BASE_DISTANCE",
-            "fee": 4000
-          },
-          {
-            "name": "ANCILLARY_SURGE_FEE"
-          }
-        ],
-        "totalFee": 4000
-      },
-      "parentId": "166",
-      "avgRatingString": "4.4",
-      "totalRatingsString": "1K+",
-      "sla": {
-        "deliveryTime": 20,
-        "lastMileTravel": 2,
-        "serviceability": "SERVICEABLE",
-        "slaString": "20 mins",
-        "lastMileTravelString": "2.0 km",
-        "iconType": "ICON_TYPE_EMPTY"
-      },
-      "availability": {
-        "nextCloseTime": "2023-10-08 03:00:00",
-        "opened": true
-      },
-      "badges": {
+//     },
+//     "cta": {
+//       "link": "swiggy://menu?restaurant_id=614573",
+//       "text": "RESTAURANT_MENU",
+//       "type": "DEEPLINK"
+//     }
+//   },
+//   {
+//     "info": {
+//       "id": "498382",
+//       "name": "Burger King",
+//       "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+//       "locality": "M2K Mall",
+//       "areaName": "Rohini",
+//       "costForTwo": "₹350 for two",
+//       "cuisines": [
+//         "Burgers",
+//         "American"
+//       ],
+//       "avgRating": 4.4,
+//       "feeDetails": {
+//         "restaurantId": "498382",
+//         "fees": [
+//           {
+//             "name": "BASE_TIME"
+//           },
+//           {
+//             "name": "BASE_DISTANCE",
+//             "fee": 4000
+//           },
+//           {
+//             "name": "ANCILLARY_SURGE_FEE"
+//           }
+//         ],
+//         "totalFee": 4000
+//       },
+//       "parentId": "166",
+//       "avgRatingString": "4.4",
+//       "totalRatingsString": "1K+",
+//       "sla": {
+//         "deliveryTime": 20,
+//         "lastMileTravel": 2,
+//         "serviceability": "SERVICEABLE",
+//         "slaString": "20 mins",
+//         "lastMileTravelString": "2.0 km",
+//         "iconType": "ICON_TYPE_EMPTY"
+//       },
+//       "availability": {
+//         "nextCloseTime": "2023-10-08 03:00:00",
+//         "opened": true
+//       },
+//       "badges": {
         
-      },
-      "isOpen": true,
-      "type": "F",
-      "badgesV2": {
-        "entityBadges": {
-          "textBased": {
+//       },
+//       "isOpen": true,
+//       "type": "F",
+//       "badgesV2": {
+//         "entityBadges": {
+//           "textBased": {
             
-          },
-          "imageBased": {
+//           },
+//           "imageBased": {
             
-          },
-          "textExtendedBadges": {
+//           },
+//           "textExtendedBadges": {
             
-          }
-        }
-      },
-      "aggregatedDiscountInfoV3": {
-        "header": "EVERY ITEM",
-        "subHeader": "@ ₹129"
-      },
-      "differentiatedUi": {
-        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-        "differentiatedUiMediaDetails": {
-          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
-          "lottie": {
+//           }
+//         }
+//       },
+//       "aggregatedDiscountInfoV3": {
+//         "header": "EVERY ITEM",
+//         "subHeader": "@ ₹129"
+//       },
+//       "differentiatedUi": {
+//         "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//         "differentiatedUiMediaDetails": {
+//           "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+//           "lottie": {
             
-          },
-          "video": {
+//           },
+//           "video": {
             
-          }
-        }
-      },
-      "reviewsSummary": {
+//           }
+//         }
+//       },
+//       "reviewsSummary": {
         
-      },
-      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-      "restaurantOfferPresentationInfo": {
+//       },
+//       "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//       "restaurantOfferPresentationInfo": {
         
-      }
-    },
-    "analytics": {
+//       }
+//     },
+//     "analytics": {
       
-    },
-    "cta": {
-      "link": "swiggy://menu?restaurant_id=498382",
-      "text": "RESTAURANT_MENU",
-      "type": "DEEPLINK"
-    }
-  }]
+//     },
+//     "cta": {
+//       "link": "swiggy://menu?restaurant_id=498382",
+//       "text": "RESTAURANT_MENU",
+//       "type": "DEEPLINK"
+//     }
+//   }]
 
 function Restaurantcard (props) {
 
@@ -257,7 +258,7 @@ function filterRestaurant(a,allrestaurant){
 
 
 function Container() {
-    const [allrestaurant, setallrestaurant] = useState(restaurantlist)
+    const [allrestaurant, setallrestaurant] = useState([])
     const [restaurant,setrestaurant] = useState(allrestaurant);
     const [a,seta] = useState("");
 
@@ -272,13 +273,13 @@ function Container() {
       const json = await data.json();
       // console.log(json.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
       setallrestaurant(json.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
-      setrestaurant(allrestaurant);
+      setrestaurant(json.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
       // console.log("Use Effect");
     }
 
     // console.log("container",allrestaurant);
 
-    return (
+    return ( allrestaurant.length===0 )?<Shimmer></Shimmer>:
       <>
         <div className="search">
           <input
@@ -309,7 +310,7 @@ function Container() {
           })}
         </div>
       </>
-    );
+    
 }
 
 export default Container;
---
+++ src/Shimmer.js
@@ -0,0 +1,14 @@
+function Shimmer() {
+    return ( 
+        <div className="shimmer">
+            <h1>Loading shimmer ui</h1>
+            <div className="Container">
+                <div className="card">Loading.....</div>
+                <div className="card">Loading.....</div>
+                <div className="card">Loading.....</div>
+            </div>
+        </div>
+     );
+}
+
+export default Shimmer;
