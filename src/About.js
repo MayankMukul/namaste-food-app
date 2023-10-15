0971324372aa62db9c62f7ede8047c38e4508f4f
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-10-15 23:36:56 +0530

    About page added

---
+++ src/About.js
@@ -0,0 +1,11 @@
+
+const About = ()=>{
+    return (
+      <>
+        <h1>ABOUT PAGE</h1>
+        <h2>This is about us page</h2>
+      </>
+    );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import ReactDOM from 'react-dom/client';
 import Navbar from './Navbar';
 import Container from './Container';
 import Footer from './Footer';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './About.js'
 
-const Applayout = (
+
+const Applayout = ()=>{
+  return (
     <>
     <Navbar></Navbar>
     <Container></Container>
     <Footer></Footer>
-    
     </>
+    );
+};
 
-);
-
+const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element:<Applayout/>
+    },
+    {
+        path:'/about',
+        element:<About/>
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(Applayout) 
+root.render(<RouterProvider router={appRouter}/>) 
