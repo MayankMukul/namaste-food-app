f679a430552c69ac1ded8437cc5013cb43e2aed7
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-10-31 17:32:56 +0530

    state in class

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,24 @@
 import React from 'react';
 class Profile extends React.Component {
-    render(){
+    constructor(props){
+        super(props);
+        this.state={
+            count:0,
+        }
+    }
+
+    render(props){
+        
         return (
             <>
             <h1>this is a profile section .</h1>
+            <h2>{this.props.name}</h2>
+            <h2>count = {this.state.count}</h2>
+            <button onClick={()=>{
+                this.setState({
+                    count:1,
+                })
+            }}>count</button>
             </>
         )
     }
