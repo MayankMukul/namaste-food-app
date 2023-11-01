43987fe231451ff0e2dcb0d79864622c5fbe5bf0
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-11-01 17:16:32 +0530

    user info

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,8 +3,24 @@ class Profile extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            count:0,
+            userInfo : {
+                login : "Dummy",
+                avatar_url : "dummy image",
+            }
         }
+    }
+
+   async componentDidMount(){
+        
+        let data = await fetch("https://api.github.com/users/MayankMukul");
+        let json = await data.json();
+        // console.log(json);
+
+        this.setState({
+            userInfo : json,
+        })
+
+        
     }
 
     render(props){
@@ -12,13 +28,9 @@ class Profile extends React.Component {
         return (
             <>
             <h1>this is a profile section .</h1>
-            <h2>{this.props.name}</h2>
-            <h2>count = {this.state.count}</h2>
-            <button onClick={()=>{
-                this.setState({
-                    count:1,
-                })
-            }}>count</button>
+            <img src={this.state.userInfo.avatar_url} alt="profile pic"/>
+            <h2>User Name : {this.state.userInfo.login}</h2>
+            
             </>
         )
     }
