53143ec900ce86e460dbf3079687af04d62341a2
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-11-04 16:28:08 +0530

    restaurantcard component

---
+++ src/components/Restaurantcard.js
@@ -0,0 +1,31 @@
+import {Link } from 'react-router-dom';
+
+function Restaurantcard (props) {
+
+    const imgurl = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+      return (
+          <div className="card">
+              {/* <img src="https://b.zmtcdn.com/data/pictures/5/19136275/c07152265062c9600e3bd6d7bf03fdc6_o2_featured_v2.jpg" alt="Image" />
+              <h3>Restaurant Name</h3>
+              <h3>Dishes</h3>
+              <h4>stars</h4> */}
+  
+              {/* <img src={restaurantlist[0].img} alt="not rendered" />
+              <h3>{restaurantlist[0].name}</h3>
+              <h3>{restaurantlist[0].dishes}</h3>
+              <h4>{restaurantlist[0].stars}</h4> */}
+              
+              <img src={imgurl + props.info.cloudinaryImageId} alt="not rendered" />
+  
+              <Link to={"/Restaurant/"+ props.info.name}>
+              <h3>{props.info.name}</h3>
+              <h3>{props.info.cuisines.join(", ")}</h3>
+              <h4>{props.info.avgRating}</h4>
+              </Link>
+  
+          </div>
+      )
+  
+}
+
+export default Restaurantcard
