ad80241c86249622d80820ab70a108d2fa91b7ca
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-11-08 13:09:15 +0530

    shimmer update

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,10 +1,11 @@
 function Shimmer() {
+    console.log("loading shimmer");
     return ( 
         <>
         {/* // <div className="shimmer"> */}
-            <div className="shimmerSearch"></div>
-            <div className="container">
-                {Array(10).fill("").map((e,id)=><div className="shimmercard" key={id}></div>)}
+            <div className="shimmerSearch w-5/6 h-10 mx-auto my-5 bg-slate-300"></div>
+            <div className=" flex flex-wrap m-auto w-11/12">
+                {Array(10).fill("").map((e,id)=><div className="shimmercard w-52 h-48 m-5 p-3 bg-slate-300" key={id}></div>)}
             </div>
         {/* // </div> */}
         
