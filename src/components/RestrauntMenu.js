d5a5823f906d48b7735d634ee20cc1d26da2d511
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-11-10 21:08:27 +0530

    update

--- src/components/RestrauntMenu.js
+++ src/components/RestrauntMenu.js
@@ -11,12 +11,12 @@ const RestrauntMenu = ()=>{
     
 
     return(
-        <>
-        <h1>Welcome To {resname}</h1>
-        <h2>Order your Food from {resname}</h2>
+        <div className="bg-slate-300 m-5 p-4">
+        <h1 className="bg-black text-white p-2 font-bold">Welcome To {resname}</h1>
+        <h2 className="text-lg font-semibold">Order Food from</h2>
         {/* <h2>Restraunt id : { resid }</h2> */}
-        <h2>Restraunt Name : {resname}</h2>
-        </>
+        <h2><span className="font-bold">Restraunt Name </span>: {resname}</h2>
+        </div>
     )
 }
 
